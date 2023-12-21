d482a2f4ca0a122de314ba872657a74bc9e23afd
MdAkash3135 <mdeaqubmia@gmail.com>
2023-12-21 20:47:37 +0600

    My first -am commit

--- playground/views.py
+++ playground/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 def say_hello(request):
     print('Debug has started and git has deleed')
+    print('Git added in this project and initilaize now')
     return render(request, 'hello.html', {'name': 'akash'})
